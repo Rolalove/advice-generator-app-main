5fb149a14e02b8a1747ba0f5289fc6f5d88db85c
kofoworola shonuyi <skofoworola3@gmail.com>
2024-04-06 00:37:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('button')
+
+
+const fetchAdvice = async () => {
+      const res = await fetch("https://api.adviceslip.com/advice");
+      const data = await res.json()
+
+      console.log(data)
+      document.getElementById('title').innerHTML = `Advice #${data.slip.id}`
+      document.getElementById("text").innerHTML = `"${data.slip.advice}"`
+}
+
+btn.addEventListener("click", function () {
+  fetchAdvice();
+});
+
+window.onload = () => {
+  fetchAdvice();
+};
